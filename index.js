9ebb85c5b707762cdaa1e096c48a390326dcc67b
heraphim <andi_bvro@yahoo.com>
2023-09-28 21:28:04 +0300

    added endpoints

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 const express = require('express');
-const cors = require('cors')
+const fs = require('fs')
+const cors = require('cors');
+require('dotenv').config();
 const mongoose = require('mongoose');
-const Task = require('./src/models/Task');
 mongoose.Promise = global.Promise;
+const Task = require('./src/models/Task');
 
-const mongoDB = 'mongodb+srv://taskuri:goitAREmere@cluster0.5wbf4ry.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGO_URL;
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
+app.listen(port, () => {
+  console.log('A pornit serverul');
+  mongoose.connect(mongoDB).then(() => {
+    console.log('Ne-am conectat la baza de date');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Functioneaza!');
+});
 
 app.get('/tasks', async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
 });
 
-app.post('/tasks', async (req, res) => {
-  const task = req.body;
-  res.status(201).json(task);
+app.delete('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+  const raspuns = await Task.findByIdAndRemove(_id);
+  res.json(raspuns);
+  // res.json({ _id: req.params.id });
 });
 
-app.delete('/tasks/:id', async (req, res) => {
-  res.json({ _id: req.params.id });
-});
+app.post('/tasks', async (req, res) => {
+  const taskToCreate = req.body;
+  const newTask = await Task.create(taskToCreate);
+  res.status(201).json(newTask);
+})
 
 app.patch('/tasks/:id', async (req, res) => {
-  const newText = req.body.text;
-  res.json({ _id: req.params.id, text: newText });
-});
+  const taskToUpdate = req.body;
+  console.log(taskToUpdate);
+  const _id = req.params.id;
+  await Task.findByIdAndUpdate(_id, taskToUpdate);
+  const freshTask = await Task.findById(_id);
+  res.json(freshTask);
+})
+
+// app.post('/tasks', async (req, res) => {
+//   const task = req.body;
+//   res.status(201).json(task);
+// });
+
+
+
+// app.patch('/tasks/:id', async (req, res) => {
+//   const newText = req.body.text;
+//   res.json({ _id: req.params.id, text: newText });
+// });
 
 // app.get('/tasks/:id', async (req, res) => {
 //   const id = req.params.id;
@@ -40,17 +71,3 @@ app.patch('/tasks/:id', async (req, res) => {
 //   }
 //   res.json(task);
 // });
-
-
-
-
-
-
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  const connection = mongoose.connect(mongoDB);
-  connection.then(() => {
-    console.log('Connected to DB');
-  });
-});
--- src/models/Task.js
+++ src/models/Task.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const task = new mongoose.Schema(
-  {
-    text: {
-      type: String,
-      minlength: 3,
-      maxlength: 170
-    }
-  },
-  { versionKey: false, timestamps: true }
-);
-const Task = mongoose.model("task", task);
+const Schema = mongoose.Schema;
 
+const task = Schema({
+  text: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+    required: [true, 'Text is required'],
+  }
+}, { versionKey: false, timestamps: true }
+);
+// task la plural in engleza => tasks
+// cat la pluarl in engleza => cats
+// child la plural in engleza => childs 
+const Task = mongoose.model("task", task);
 module.exports = Task;
